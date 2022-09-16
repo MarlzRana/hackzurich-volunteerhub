0d273d00786319f423a755f463115fbfc1a1970c
William Lardner <william.lardner0109@gmail.com>
2022-09-16 22:52:08 +0100

    Added basic design for landing page

--- frontend/src/pages/landing/Header.js
+++ frontend/src/pages/landing/Header.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom";
+import "./landing-page.css";
+
 const Header = () => {
     return (
-        <>
-        Header
-        </>
+        <div className="landing-header">
+            <div className="landing-title">
+                <h1>VolunteerHub</h1>
+            </div>
+            <div className="landing-nav">
+                <p>An Organisation?</p>
+                <Link to="/">Sign Up</Link>
+                <Link to="/">Login</Link>
+            </div>
+        </div>
     );
 }
 
--- frontend/src/pages/landing/LandingPage.js
+++ frontend/src/pages/landing/LandingPage.js
@@ -1,9 +1,19 @@
 import Header from './Header';
+import "./landing-page.css";
 
 const LandingPage = () => {
     return (
         <>
-            landing
+            <Header />
+            <div className="landing-hero">
+                <div className="landing-hero-left">
+                    <h2>Connecting Volunteers With Top-Tier Organisations</h2>
+                    <p>We connect proactive volunteers with amazing organisations that are focussed on making the world a better place.</p>
+                </div>
+                <div className="landing-hero-right">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Love_Heart_SVG.svg/2258px-Love_Heart_SVG.svg.png" />
+                </div>
+            </div>
         </>
     );
 }
