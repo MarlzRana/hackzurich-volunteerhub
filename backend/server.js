fd1f4397106e7ec0b8d3d19bcac2dbb34963b652
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2022-09-16 22:56:28 +0100

    basic barebones api working and created gitignore

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,44 @@
-// Packages
-require("dotenv").config()
+// PACKAGES
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Routes
-
+// ROUTES
 
 // Creating main app object
 const app = express();
 
-
-// Middleware
+// MIDDLEWARE
 app.use("/public", express.static(process.cwd() + "/public"));
 // Setting up the CORS policy
-app.use(cors({
-    origin: "*"
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
+// MAIN LOGIC
+app.route("/").get((req, res) => {
+  res.send("Welcome to VolunteerHub");
+});
 
+// 404 Not Found "Middleware"
+app.use((req, res, next) => {
+  res.status(404).type("text").send("Page not found, try again!");
+});
 
+// PORT AND LISTEN SETUP
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+module.exports = app;
